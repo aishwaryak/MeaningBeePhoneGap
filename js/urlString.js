478e416f99ef931e3ccbe6791cd88b87abc1b013
jayakumaur <jaya.kumaur@gmail.com>
2015-10-25 22:18:17 -0400

    addition of aws url

--- js/urlString.js
+++ js/urlString.js
@@ -1,5 +1,7 @@
 /**
  * 
  */
-var urlString = "http://127.0.0.1:8000";
-urlString = "http://ec2-54-148-143-69.us-west-2.compute.amazonaws.com:8000";
+var urlString;
+// urlString = "http://127.0.0.1:8000";
+//AWS URL as on Oct 25
+urlString = "http://52.89.41.254:8000/";
